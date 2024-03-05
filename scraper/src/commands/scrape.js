4a3a10efbdc9f97b007a2bbadac78d3cf05afa8c
jacob <jacoobes@sern.dev>
2024-03-05 12:47:05 -0600

    scraper  mvp

--- scraper/src/commands/scrape.js
+++ scraper/src/commands/scrape.js
@@ -1,22 +1,28 @@
 import { commandModule, CommandType, Service } from '@sern/handler'
 import { writeFileSync } from 'fs'
 import { requirePermission } from '../plugins/requirePermission.js'
+import { PermissionsBitField } from 'discord.js'
 const logger = Service('@sern/logger')
+
 export default commandModule({
     type: CommandType.Text,
-    plugins: [requirePermission('bot', 'VIEW_CHANNEL', 'Bot does not have permissions in this server'  )],
+    plugins: [requirePermission('bot', [PermissionsBitField.Flags.ViewChannel], 'Bot does not have permissions in this server'  )],
     execute: async (ctx) => {
         const message = ctx.message;
-        const textChannels = message.guild.channels.cache.filters(channel => channel.type === 'text');
-
+        const textChannels = await message
+            .guild
+            .channels
+            .fetch()
+            .then(chanels => chanels.filter((a,b) => a.isTextBased()))
+        
         let allMessages = [];
         //Iterate over all text channels
         for (const channel of textChannels.values()) {
-            if (!channel.permissionsFor(message.guild.me).has('VIEW_CHANNEL')) {
+            if (!channel.permissionsFor(message.client.user).has('VIEW_CHANNEL')) {
                 console.log(`Bot does not have permissions to view channel ${channel.name}`);
                 continue; //move on to next channel
             }
-            const messages = channel.messages.fetch({ limit: 100 }); //Adjustable limit
+            const messages = await channel.messages.fetch({ limit: 100 }); //Adjustable limit
             messages.forEach(message => {
                 allMessages.push(`${message.author.username}: ${message.content}\n`);
             });
