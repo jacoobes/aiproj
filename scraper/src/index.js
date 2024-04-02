8819b566001678d04939d73cf6bd87bf507721f0
Zachary Pham <elswordmaster15@gmail.com>
2024-04-02 10:28:47 -0500

    My glasses broke

--- scraper/src/index.js
+++ scraper/src/index.js
@@ -42,19 +42,25 @@ client.on('message', async message => {
             return console.log('Bot does not have the correct permissions in the server');
         }
         const textChannels = message.guild.channels.cache.filters(channel => channel.type === 'text');
-
-        let allMessages = [];
-        //Iterate over all text channels
         for (const channel of textChannels.values()) {
             if (!channel.permissionsFor(message.guild.me).has('VIEW_CHANNEL')) {
                 console.log(`Bot does not have permissions to view channel ${channel.name}`);
                 continue; //move on to next channel
-            }
-            const messages = await channel.messages.fetch({ limit: 100 }); //Adjustable limit
-            messages.forEach(message => {
-                allMessages.push(`${message.author.username}: ${message.content}\n`);
-            });
+        }
+        let allMessages = [];
+        let lastMessageId = null;
+        let messageFetched = 0;
+        let fetchLimiter = 100;
 
+        do {
+            const messages = await channel.messages.fetch({ limit: fetchLimiter, before: lastMessageId });
+            messages.forEach(message => {
+                const formattedMessage = `${message.author.username} (${new Date(message.createdTimeStamp).toLocaleString()}): ${message.content}\n`
+                allMessages.push(formattedMessage);
+            });
+            lastMessageId = messages.last().id;
+            messageFetched += messages.size;
+        } while (messages.size === fetchLimiter $$ messageFetched < 3000); //Until it hits 3000
             const textContent = allMessages.join('');
 
             const fs = require('fs');
