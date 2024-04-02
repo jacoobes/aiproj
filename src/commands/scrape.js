a66de934e3dd6a752c40f030da2cc2a35237d861
jacob <jacoobes@sern.dev>
2024-04-02 12:25:24 -0500

    fix merge issues

--- src/commands/scrape.js
+++ src/commands/scrape.js
@@ -17,33 +17,37 @@ export default commandModule({
             .channels
             .fetch()
             .then(chanels => chanels.filter((a) => a.isTextBased()))
-
+        console.log(textChannels)
+        let allMessages = [];
         //Iterate over all text channels
         for (const channel of textChannels.values()) {
-                    if (!channel.permissionsFor(message.client.user).has(PermissionsBitField.Flags.ViewChannel)) {
-                        console.log(`Bot does not have permissions to view channel ${channel.name}`);
-                        continue; //move on to next channel
+                if (!channel.permissionsFor(message.client.user).has(PermissionsBitField.Flags.ViewChannel)) {
+                    console.log(`Bot does not have permissions to view channel ${channel.name}`);
+                    continue; //move on to next channel
                 }
-                let allMessages = [];
                 let lastMessageId = null;
                 let messageFetched = 0;
                 let fetchLimiter = 100;
-
+                let done  = false 
                 do {
-                    const messages = await channel?messages.fetch({ limit: fetchLimiter, before: lastMessageId });
+                    const messages = await channel?.messages.fetch({ limit: fetchLimiter, before: lastMessageId });
                     messages.forEach(message => {
-                        const formattedMessage = `${message.author.username} (${new Date(message.createdTimeStamp).toLocaleString()}): ${message.content}\n`
+                        const formattedMessage = `${message.author.username} (${new Date(message.createdTimeStamp).toLocaleString()}): ${message.content}`
                         allMessages.push(formattedMessage);
                     });
-                    lastMessageId = messages.last().id;
-                    messageFetched += messages.size;
-                } while (messages.size === fetchLimiter $$ messageFetched < 3000); //Until it hits 3000
-                    const textContent = allMessages.join('');
-                    const dir = `guilddata/`
-                    mkdirSync(dir, { recursive: true });
-                    writeFileSync(dir+`${message.guild.id}_chat_history.txt`, textContent, { encoding: 'utf8' });
-                    logger.info({ message: `Saved to ${message.guild.id}_chat_history.txt` });
-                }
-           }
-    })
+                    if(messages.last()) {
+                        lastMessageId = messages.last().id;
+                        messageFetched += messages.size;
+                    } else {
+                        done = true;
+                    }
+                } while (!done && messageFetched < 3000); //Until it hits 3000
+        }
+        const textContent = allMessages.join('\n');
+        const dir = `guilddata/`
+        mkdirSync(dir, { recursive: true });
+        writeFileSync(dir+`${message.guild.id}_chat_history.txt`, textContent, { encoding: 'utf8' });
+        logger.info({ message: `Saved to ${message.guild.id}_chat_history.txt` });
+    }
+})
 
